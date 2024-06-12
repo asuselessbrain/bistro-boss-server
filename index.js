8543ae69d2170e376d2a1ca9d60add1abeceaa59
asuselessbrain <shovon1023456789@gmail.com>
2024-06-12 23:16:47 +0600

    get all menu

--- index.js
+++ index.js
@@ -25,8 +25,13 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-    const menuCollection = client.db("sample_mflix");
-    const movies = database.collection("movies");
+    const menuCollection = client.db("bestroBoss").collection("menu");
+
+    app.get('/menus', async (req, res) => {
+        const result = await menuCollection.find().toArray();
+
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
