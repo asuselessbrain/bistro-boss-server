59567851917de60866db8c401b9fea3eb1867128
asuselessbrain <shovon1023456789@gmail.com>
2024-06-13 12:29:03 +0600

    testimonial data load successfull

--- index.js
+++ index.js
@@ -26,12 +26,21 @@ async function run() {
     await client.connect();
 
     const menuCollection = client.db("bestroBoss").collection("menu");
+    const reviewsCollection = client.db("bestroBoss").collection("reviews");
 
     app.get('/menus', async (req, res) => {
         const result = await menuCollection.find().toArray();
 
         res.send(result)
     })
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
+
+      res.send(result)
+  })
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
