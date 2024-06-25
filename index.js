4dea0d2b265198d7d61f07adbf4044206754961f
asuselessbrain <shovon1023456789@gmail.com>
2024-06-26 00:27:49 +0600

    delete item from my cart is completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -49,14 +49,27 @@ async function run() {
 
     // cart collection
 
-    app.get('/carts', async(req, res) => {
-      const result = await cartsCollection.find().toArray();
-      res.send(result)
-    })
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+
+      const query = { email: email };
+      const result = await cartsCollection
+        .find(query)
+        .sort({ _id: -1 })
+        .toArray();
+      res.send(result);
+    });
 
     app.post("/carts", async (req, res) => {
       const cartItem = req.body;
       const result = await cartsCollection.insertOne(cartItem);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartsCollection.deleteOne(query);
       res.send(result);
     });
 
